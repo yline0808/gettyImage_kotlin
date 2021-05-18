130795e61f1ea4b190b16b9c1948b73a0d2366a0
류요선 <ryuyoseon@rysMAC.local>
2021-05-18 22:19:55 +0900

    [추가] main 세부 코드 추가

--- app/src/main/java/net/ddns/rsupporttest/MainActivity.kt
+++ app/src/main/java/net/ddns/rsupporttest/MainActivity.kt
@@ -14,17 +14,41 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
+/*
+-inflate
+xml에 있는 레이아웃을 객체화 하는 행동
+setContentView() 함수가 xml을 객체화 하는 inflate행동
+
+-content
+-activity
+화면 UI를 담당하는 컴포넌
+
+-context
+시스템을 사용하기 위한 정보(프로퍼티)와 두구(메서드)가 담겨있는 클래스
+컴포넌트 실행시 함께 생성
+앱에서 사용하는 기본기능이 담긴 기본 클래스
+
+-스코프 함수 (this[run, apply, with], it[let, also])
+run : this를 생략 가능
+let : it를 생략 불가능 하지만 다른 이름으로 변경 가능
+ */
+
 class MainActivity : AppCompatActivity() {
+    // 늦은 초기화 불리는 시점에 괄호 안에 거로 초기화, 세이프 콜 방지
+    // 레이아웃 인플레이터를 바인딩된 코틀린 메인 클래스로 변환하여 초기화
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    // 여기서는 rowimgsrc에 null이 들어갈 수 있기 때문에 lazy 안씀
     private val rowImgList:ArrayList<RowImgSrc?> = ArrayList<RowImgSrc?>()
     private val imgAdapter:ImgAdapter = ImgAdapter(rowImgList)
+    // 이미 로딩중에 다시 로딩을 하기 않기 위함
     private var isLoading:Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 바인딩 된 root뷰를 contentView에 전달
         setContentView(binding.root)
 
-        // recyclerView 세팅
+        // recyclerView 세팅 this:recyclerView
         binding.recyclerView.run {
             layoutManager = LinearLayoutManager(context)
             adapter = imgAdapter
@@ -37,6 +61,7 @@ class MainActivity : AppCompatActivity() {
 
     // 스크롤 리스너
     private val scrollListener = object:RecyclerView.OnScrollListener(){
+        // dx, dy는 위치가 아닌 얼마나 스크롤 되었는지
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
             super.onScrolled(recyclerView, dx, dy)
 
@@ -55,12 +80,16 @@ class MainActivity : AppCompatActivity() {
 
     // 로딩세팅 & 이미지 src 가져오는 함수
     private fun getRowItemList() {
+        //launch를 통해 상태관리 가능 async는 연산 결과도 받을 수 있음
         CoroutineScope(Dispatchers.Main).launch {
+            //나중에 로딩의 위치를 기억해서 지우기 위함
             val loadingIdx:Int = rowImgList.size
             rowImgList.add(null)
             imgAdapter.notifyItemInserted(loadingIdx)
 
             // rowImgList에 새로운 항목 추가
+            // 코루틴 스코프 안에서 작성하여서 백그라운드 실행처럼 동작
+            // suspend 키워드가 있는 함수를 사용하여 해당 함수가 끝나야 다음 동작 실
             val isEnd:Boolean = fetchImgSrc(rowImgList)
 
             rowImgList.removeAt(loadingIdx)
@@ -75,7 +104,8 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-// jsoup을 사용한 이미지 src 파싱 & 아이템 배열 추가
+// jsoup을 사용한 이미지 src 파싱 & 아이템 배열 추가행
+// suspend를 사용한 곳에서 다른 Dispatchers가능, withContext로 다른 Dispatchers로 변경 가능
 suspend fun fetchImgSrc(rowImgList:ArrayList<RowImgSrc?>):Boolean = withContext(Dispatchers.IO) {
     val WEB_URL = "https://www.gettyimages.com/photos/collaboration?page=%d&phrase=collaboration&sort=mostpopular"
     val pageNum:Int = if (rowImgList.size <= 1) 1 else (rowImgList.size - 1) / 20 + 1
